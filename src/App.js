7e53fc673b200e11f03fc3af9645ec36a4822edd
田中　龍 <tanaka@anagram-works.jp>
2024-01-24 10:59:50 +0900

    完了したタスクの削除

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
     // 入力した値を取得
     const name = todoNameRef.current.value;
 
+    // 名前が空の場合は追加しない
+    if(name === '') return;
+
     setTodos((prevTodos) => {
       // スプレッド構文で現在の配列を展開し、新たに追加するタスクを最後尾に追加
       return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
@@ -25,13 +28,19 @@ function App() {
     todo.completed = !todo.completed;
     setTodos(newTodos);
   }
+
+  const handleClear = () => {
+    // 未完了のタスクのみを抽出
+    const newTods = todos.filter((todo) => !todo.completed);
+    setTodos(newTods);
+  }
   
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
-      <button>完了したタスクの削除</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
       <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </>
   );
